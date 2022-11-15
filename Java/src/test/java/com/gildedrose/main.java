a5e75adb516cb20d34fbfb5e0aaa7d25fe77af11
TheHerbin <59963115+TheHerbin@users.noreply.github.com>
2022-11-15 10:22:24 +0100

    TP 5

--- Java/src/test/java/com/gildedrose/main.java
+++ Java/src/test/java/com/gildedrose/main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("test");
     }
 
     public void updateQuality(ArrayList<Item> itemList){
